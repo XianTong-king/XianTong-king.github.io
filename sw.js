8dff5ee91762161a9c1a9150d32abba69e3d880a
XianTong-king <3089785968@qq.com>
2024-11-24 10:42:01 +0800

    Site updated: 2024-11-24 10:42:01

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732415179373";
+const VERSION = "1732416115185";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
