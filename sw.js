3766cabc6ad26d10d196202e855d2d05428be05b
XianTong-king <3089785968@qq.com>
2025-04-08 16:06:48 +0800

    Site updated: 2025-04-08 16:06:47

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1744099303155";
+const VERSION = "1744099603275";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
