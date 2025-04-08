7a3e45d05d58eb0472b720a23d02b436b3b36b53
XianTong-king <3089785968@qq.com>
2025-04-08 16:47:15 +0800

    Site updated: 2025-04-08 16:47:14

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const preCache = [
   "/images/taichi.png",
-  "/",
+  "/images/banner.webp",
   "/images/taichi-fill.png",
   "/css/loader.css",
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1744099603275";
+const VERSION = "1744102028152";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
